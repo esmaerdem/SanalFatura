cd37679f053b6103ed8ab90db1dd6697e28a822e
Esma Nur <esmanurerdem99@gmail.com>
2021-08-06 17:18:54 +0300

    Add project files.

---
+++ SanalFaturaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SanalFaturaMVC.MvcApplication" Language="C#" %>
---
+++ SanalFaturaMVC/Models/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SanalFaturaMVC.Models
+{
+    public class Email
+    {
+        public string Ad { get; set; }
+        public string Telefon { get; set; }
+        public string Mail { get; set; }
+        public string Konu { get; set; }
+        public string Mesaj { get; set; }
+    }
+
+    public class Message
+    {
+        public bool IsSuccess { get; set; }
+
+        public string message { get; set; }
+    }
+}
